415526b20b08545c90e500b4784b6ee81978da6b
Jeffrey Killeen <jtk12b@acu.edu>
2014-10-05 23:21:20 -0500

    Made more changes to Beta Design Document

--- project/index.rb
+++ project/index.rb
@@ -5,6 +5,24 @@ get '/' do
 	erb :home
 end
 
-get '/tyler' do
-	"Tyler sucks"
+get '/ice' do
+	@var = "thing"
+	erb :ice
+end
+
+get '/login' do
+	erb :login
+end
+
+post '/login' do
+	@username = this.username
+	@password = this.password
+	@error = ""
+	users = User.all(:where => password == @password)
+	if(users.nil?)
+		@error = "Error: Wrong credentials"
+		redirect '/login'
+	else
+		redirect '/'
+	end
 end
