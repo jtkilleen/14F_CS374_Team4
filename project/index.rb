5b374e444aa89e43363f05e988e9be998a05e0e5
Jeffrey Killeen <jtk12b@acu.edu>
2014-10-13 14:30:11 -0500

    Added changes to login

--- project/index.rb
+++ project/index.rb
@@ -1,10 +1,26 @@
 require 'csv'
 
+enable :sessions
+
 get '/' do
+	puts session[:username]
+	puts
 	@var = "jonathan"
 	erb :home
 end
 
 get '/login' do
+	@errors = ""
+	erb :login
+end
+
+post '/login' do
+	email = params[:email]
+	session[:username] = params[:email]
+	if email == "jeff@jeff.com"
+		redirect '/'
+	end
+	session[:username] =
+	@errors = "Error, could not login"
 	erb :login
 end
