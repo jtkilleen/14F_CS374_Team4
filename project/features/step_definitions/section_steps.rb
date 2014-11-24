f38f2169f8edade13d33688b94b71a29d1320d5c
Jeffrey Killeen <jtk12b@acu.edu>
2014-11-24 00:01:18 -0600

    Added Steps again

--- project/features/step_definitions/section_steps.rb
+++ project/features/step_definitions/section_steps.rb
@@ -6,24 +6,7 @@ Given(/^I am on the "(.*?)" building page$/) do |building|
 end
 
 When(/^I move a class with crn "(.*?)" to room "(.*?)" and time "(.*?)"$/) do |crn, room, time|
-	@conflicts = []
-	section = Section.where(crn: "#{crn}").first
-	#teacher = section.teachers.first
-	room = Room.where(building: @building, roomnumber: room).first
-	time = time.split("-")[0].gsub(':','')
-	@canMove = true
-	days = section.day.split("")
-	room.sections.each do |s|
-		dayCheck = false
-		days.each do |d|
-			if s.day.include? d
-				dayCheck = true
-			end
-		end
-		if s.beginTime == time  && dayCheck
-			@canMove = false
-		end
-	end
+	classSearch(crn, room, time)
 end
 
 Then(/^it should say "(.*?)"$/) do |output|
